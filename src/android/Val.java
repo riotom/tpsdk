fe70202d4137f324af3b230fbb328bb06b55ccb7
Zikov Roman <beeline.mts@gmail.com>
2019-09-03 21:15:55 +0300

    Add files via upload

---
+++ src/android/Val.java
@@ -0,0 +1,16 @@
+package com.rdt.tpsdk;
+
+public class Val {
+    public static String PACKAGE_NAME = "com.rdt.tpsdk";
+	
+    public static String BLOCKCHAIN = "EOS";
+    public static String DAPP_NAME = "MyDAPP";
+    public static String DAPP_ICON = "https://test.net/img/logofav1.png";
+    public static String DAPP_MEMO = "MyDAPP based on EOS";
+    public static String DAPP_URL = "https://test.net/";
+    public static String DAPP_ACCOUNT = "testdapp";
+
+    public static String WALLET_account = "";
+    public static String WALLET_wallet = "";
+    public static String WALLET_publickey = "";
+}
