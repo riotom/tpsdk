694a25a292d079c1ec69dce07a07c01ccd9c4320
Zikov Roman <beeline.mts@gmail.com>
2019-11-04 19:56:58 +0300

    Update Val.java

--- src/android/Val.java
+++ src/android/Val.java
@@ -4,11 +4,11 @@ public class Val {
     public static String PACKAGE_NAME = "com.rdt.tpsdk";
 	
     public static String BLOCKCHAIN = "EOS";
-    public static String DAPP_NAME = "Crypto Heroes";
-    public static String DAPP_ICON = "https://cryptoheroes.net/img/logofav1.png";
-    public static String DAPP_MEMO = "Crypto Heroes game based on EOS";
-    public static String DAPP_URL = "https://cryptoheroes.net/";
-    public static String DAPP_ACCOUNT = "crheroestest";
+    public static String DAPP_NAME = "Crypto";
+    public static String DAPP_ICON = "https://www.net/img/logofav1.png";
+    public static String DAPP_MEMO = "Crypto game based on EOS";
+    public static String DAPP_URL = "https://www.net/";
+    public static String DAPP_ACCOUNT = "xxxx";
 
     public static String WALLET_account = "";
     public static String WALLET_wallet = "";
